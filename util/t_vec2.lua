597e9b02563d45c2180f2ad1d4b8d86bb7fa5946
liyang <wood9366@gmail.com>
2023-02-26 09:56:26 +0800

    add vec2 test case, fix some of vec2 issues.

--- util/t_vec2.lua
+++ util/t_vec2.lua
@@ -4,35 +4,104 @@ Vec2 = require('util/vec2')
 TestVec2 = {}
 
 function TestVec2:test_new()
-  local v = Vec2:new()
-  lu.assertEquals(v, {x=0, y=0})
-end
+  local t = Vec2:new()
+  lu.assertEquals(t, {x=0, y=0})
 
-function TestVec2:test_new_with_val()
-  local v = Vec2:new{x=2, y=3}
-  lu.assertEquals(v, {x=2, y=3})
+  local a = Vec2:new{x=2, y=3}
+  lu.assertEquals(a, {x=2, y=3})
 
-  v = Vec2:new{x=7}
-  lu.assertEquals(v, {x=7, y=0})
+  local b = Vec2:new{x=7}
+  lu.assertEquals(b, {x=7, y=0})
 
-  v = Vec2:new{y=3}
-  lu.assertEquals(v, {x=0, y=3})
+  local c = Vec2:new{y=3}
+  lu.assertEquals(c, {x=0, y=3})
+
+  local d = Vec2:new(c)
+  lu.assertEquals(d, {x=0, y=3})
+  lu.assertTrue(c ~= d)
 end
 
 function TestVec2:test_add()
   local a = Vec2:new{x=3, y=7}
   local b = Vec2:new{x=1, y=20}
 
+  lu.assertEquals(Vec2.Add(a, b), {x=4, y=27})
+
   a:add(b)
   lu.assertEquals(a, {x=4, y=27})
 end
 
-function TestVec2:test_add_static()
-  local a = Vec2:new{x=3, y=7}
-  local b = Vec2:new{x=1, y=20}
+function TestVec2:test_sub()
+  local a = Vec2:new{x=20,y=5}
+  local b = Vec2:new{x=3,y=22}
 
-  local c = Vec2.add(a, b)
-  lu.assertEquals(c, {x=4, y=27})
+  lu.assertEquals(Vec2.Sub(a, b), {x=17,y=-17})
+
+  a:sub(b)
+  lu.assertEquals(a, {x=17,y=-17})
+end
+
+function TestVec2:test_mul()
+  local a = Vec2:new{x=20,y=5}
+
+  lu.assertEquals(Vec2.Mul(a, 3), {x=60,y=15})
+
+  a:mul(3)
+  lu.assertEquals(a, {x=60,y=15})
+end
+
+function TestVec2:test_mag()
+  local a = Vec2:new{x=20,y=5}
+
+  lu.assertEquals(a:mag(), math.sqrt(a.x * a.x + a.y * a.y))
+end
+
+function TestVec2:test_norm()
+  local a = Vec2:new{x=20,y=5}
+
+  local mag = math.sqrt(a.x * a.x + a.y * a.y)
+  local ret = {x=a.x/mag, y=a.y/mag}
+
+  lu.assertEquals(Vec2.Norm(a), ret)
+  lu.assertEquals(Vec2.Norm(a):mag(), 1)
+
+  a:norm()
+  lu.assertEquals(a, ret)
+  lu.assertEquals(a:mag(), 1);
+
+  local b = Vec2:new{x=0,y=0}
+  b:norm()
+  lu.assertEquals(b, {x=0, y=0})
+end
+
+function TestVec2:test_dot()
+  local a = Vec2:new{x=1,y=0}
+  local b = Vec2:new{x=math.cos(math.pi / 3),y=math.sin(math.pi / 3)}
+
+  local ret = a:mag() * b:mag() * math.cos(math.pi / 3)
+
+  lu.assertEquals(Vec2.Dot(a, b), ret)
+  lu.assertEquals(a:dot(b), ret)
+end
+
+function TestVec2:test_cross()
+  local a = Vec2:new{x=1,y=0}
+  local b = Vec2:new{x=math.cos(math.pi / 6),y=math.sin(math.pi / 6)}
+
+  local ret = a:mag() * b:mag() * math.sin(math.pi / 6)
+
+  lu.assertAlmostEquals(Vec2.Cross(a, b), ret)
+  lu.assertAlmostEquals(Vec2.Cross(b, a), -ret)
+
+  lu.assertAlmostEquals(a:cross(b), ret)
+  lu.assertAlmostEquals(b:cross(a), -ret)
+end
+
+function TestVec2:test_distance()
+  local a = Vec2:new{x=1, y=0}
+  local b = Vec2:new{x=5, y=0}
+
+  lu.assertEquals(Vec2.Distance(a, b), 4)
 end
 
 os.exit( lu.LuaUnit.run() )
--- util/vec2.lua
+++ util/vec2.lua
@@ -3,33 +3,42 @@ local Vec2 = {}
 Vec2.__index = Vec2
 
 function Vec2.Add(a, b)
-  return Vec2:new(a).add(b)
+  return Vec2:new(a):add(b)
 end
 
 function Vec2.Sub(a, b)
-  return Vec2:new(a).sub(b)
+  return Vec2:new(a):sub(b)
 end
 
 function Vec2.Mul(v, f)
-  return Vec2:new(v).mul(f)
+  return Vec2:new(v):mul(f)
 end
 
 function Vec2.Dot(a, b)
-  return Vec2:new(a).dot(b)
+  return Vec2:new(a):dot(b)
+end
+
+function Vec2.Cross(a, b)
+  return Vec2:new(a):cross(b)
 end
 
 function Vec2.Norm(v)
-  return Vec2:new(v).norm()
+  return Vec2:new(v):norm()
 end
 
 function Vec2.Distance(a, b)
-  return Vec2:new(a).sub(b).mag()
+  return Vec2:new(a):sub(b):mag()
 end
 
 function Vec2:new(o)
   local o = o or { x = 0, y = 0 }
   o.x = o.x or 0
   o.y = o.y or 0
+
+  if getmetatable(o) == self then
+    o = { x = o.x, y = o.y }
+  end
+
   setmetatable(o, self)
   return o
 end
@@ -65,11 +74,15 @@ function Vec2:mag()
 end
 
 function Vec2:norm()
-  return self.div(self.mag())
+  return self:div(self:mag())
 end
 
 function Vec2:dot(v)
   return self.x * v.x + self.y * v.y
+end
+
+function Vec2:cross(v)
+  return self.x * v.y - self.y * v.x
 end
 
 return Vec2
