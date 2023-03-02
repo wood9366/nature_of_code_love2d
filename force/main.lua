6070527e5137bc5ee8ab345aa4dbfae64d568c3d
liyang <7325560@qq.com>
2023-03-02 17:46:39 +0800

    add ground in force ex

--- force/main.lua
+++ force/main.lua
@@ -28,20 +28,35 @@ function Mover:update(dt)
   self.velocity:add(Vec2.Mul(self.acc, dt))
   self.pos:add(Vec2.Mul(self.velocity, dt))
 
-  local width, height, _ = love.window.getMode()
-
-  if self.pos.y >= height then
-    self.velocity.y = -self.velocity.y * 0.9
-  end
-
   self.force:set_xy(0, 0)
 end
 
+function Mover:radius()
+  return self.mass * 10
+end
+
 function Mover:draw()
-  love.graphics.circle("line", self.pos.x, self.pos.y, 10)
+  love.graphics.circle("line", self.pos.x, self.pos.y, self:radius())
+end
+
+local Ground = {}
+Ground.__index = Ground
+
+function Ground:new(o)
+  local o = o or {}
+  local width, height, _ = love.window.getMode()
+  o.y = o.y or (height * 3 / 4)
+  setmetatable(o, self)
+  return o
+end
+
+function Ground:draw()
+  local width, height, _ = love.window.getMode()
+  love.graphics.line(0, self.y, width, self.y)
 end
 
 local movers = {}
+local ground = Ground:new()
 
 function love.load()
   love.window.setTitle("Nature of Code - Force")
@@ -56,12 +71,17 @@ function love.mousepressed(x, y, button)
   end
 end
 
-local g = Vec2:new{x=0,y=10}
+local g = Vec2:new{x=0,y=50}
 
 function love.update(dt)
   for _, mover in ipairs(movers) do
     mover:add_force(Vec2.Mul(g, mover.mass))
     mover:update(dt)
+
+    if mover.pos.y >= ground.y - mover:radius() then
+      mover.pos.y = ground.y - mover:radius()
+      mover.velocity.y = -mover.velocity.y * 0.9
+    end
   end
 end
 
@@ -77,7 +97,9 @@ end
 
 function love.draw()
   love.graphics.print(string.format("time: %02d:%02d:%02d", get_cur_time()), 10, 10)
+  love.graphics.print(string.format("ground: y: %.2f", ground.y), 10, 30)
 
+  ground:draw()
   for _, mover in ipairs(movers) do
     mover:draw()
   end
